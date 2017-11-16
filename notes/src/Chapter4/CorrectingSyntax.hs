a4272a756ac35141b2e08857c50dbb247a247d6e
Thomas Smith <tnsmith@live.nl>
2017-11-16 14:55:08 +0100

    Add corrections

--- notes/src/Chapter4/CorrectingSyntax.hs
+++ notes/src/Chapter4/CorrectingSyntax.hs
@@ -1,19 +1,24 @@
 module Chapter4.CorrectingSyntax where
 
 -- Ex. 1
--- x = (+)
+x = (+)
 
 -- F xs = w 'x' 1
 --      where w = length xs
+f xs = w `x` 1
+     where w = length xs
 
 
 -- Ex. 2
 -- answer = \ X = x
+answer = \ x -> x
 
 
 -- Ex. 3
 -- answer' = \ x : xs -> x
+answer' = \ (x : xs) -> x
 
 
 -- Ex. 4
--- f (a b) = A
+-- f' (a b) = A
+f' (a, b) = a
